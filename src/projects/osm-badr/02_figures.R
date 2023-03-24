6c3d2fea04287e944586b5111d20cd7d271db954
Marcus Becker <mabecker@ualberta.ca>
2023-03-24 15:43:39 -0600

    small tweaks to osm figures

--- src/projects/osm-badr/02_figures.R
+++ src/projects/osm-badr/02_figures.R
@@ -99,7 +99,7 @@ on_off |>
              position = position_jitterdodge(jitter.width = 0.2)) +
   scale_color_manual(values = veg.col) +
   scale_y_sqrt() +
-  theme_classic() +
+  theme_minimal() +
   coord_cartesian(ylim = c(0, 6)) +
   labs(x = "Placement",
        y = expression(Density~(individuals~per~km^2))) +
@@ -110,12 +110,13 @@ on_off |>
         legend.position = "top",
         legend.title = element_blank(),
         legend.margin = margin(0, 0, 0, 0),
-        legend.spacing.x = unit(0.01, "cm"),
+        legend.spacing.x = unit(0.05, "cm"),
         axis.ticks = element_blank(),
-        panel.grid.major.y = element_line(linewidth = 0.5, color = "grey80")) +
+        panel.grid.major.y = element_line(linewidth = 0.5, color = "grey80"),
+        panel.grid.major.x = element_blank()) +
   facet_grid(. ~ treatment, scales = "free_x", space = "free")
 
-ggsave(paste0(g_drive, "results/osm/figures/wtd_on-off_sqrt.png"), height = 4, width = 8, dpi = 500)
+ggsave(paste0(g_drive, "results/osm/figures/wtd_on-off_sqrt.png"), height = 4, width = 8, dpi = 500, bg = "white")
 
 # Now the 2 buffer treatments:
 
@@ -154,30 +155,36 @@ ggsave(paste0(g_drive, "results/osm/figures/wtd_buffer_sqrt.png"), height = 4, w
 
 on_off |>
   filter(common_name == "Moose") |>
+  mutate(lci_density = ifelse(mean_density > 0, lci_density, 0),
+         uci_density = ifelse(mean_density > 0, uci_density, 0)) |>
   ggplot(aes(x = fine_scale, y = mean_density)) +
-  geom_point(aes(color = vegetation),
+  geom_point(aes(color = Habitat),
              size = 3,
              position = position_dodge(width = 0.75)) +
-  geom_linerange(aes(ymin = lci_density, ymax = uci_density, color = vegetation),
+  geom_linerange(aes(ymin = lci_density, ymax = uci_density, color = Habitat),
                  linewidth = 0.5, position = position_dodge(width = 0.75)) +
   geom_point(data = (on_off_jem |> filter(common_name == "Moose")),
-             aes(x = fine_scale, y = mean_density, color = vegetation),
+             aes(x = fine_scale, y = mean_density, color = Habitat),
              size = 2,
              alpha = 0.15,
              position = position_jitterdodge(jitter.width = 0.2)) +
   scale_color_manual(values = veg.col) +
-  scale_y_sqrt(breaks = c(0, 1, 2, 3, 4, 5, 6)) +
-  theme_classic() +
+  scale_y_sqrt() +
+  theme_minimal() +
+  coord_cartesian(ylim = c(0, 3)) +
   labs(x = "Placement",
        y = expression(Density~(individuals~per~km^2))) +
-  coord_cartesian(ylim = c(0, 3)) +
   theme(axis.text.x = element_text(angle = 0, hjust = 0.5),
         axis.title.x = element_text(size = 14, margin = margin(0.4, 0, 0, 0, unit = "cm")),
-        axis.title.y = element_text(size = 12, margin = margin(0, 0.4, 0, 0, unit = "cm")),
+        axis.title.y = element_text(size = 10, margin = margin(0, 0.4, 0, 0, unit = "cm")),
         strip.text = element_text(size = 11),
-        legend.position = "none",
+        legend.position = "top",
+        legend.title = element_blank(),
+        legend.margin = margin(0, 0, 0, 0),
+        legend.spacing.x = unit(0.05, "cm"),
         axis.ticks = element_blank(),
-        panel.grid.major.y = element_line(linewidth = 0.5, color = "grey80")) +
+        panel.grid.major.y = element_line(linewidth = 0.5, color = "grey80"),
+        panel.grid.major.x = element_blank()) +
   facet_grid(. ~ treatment, scales = "free_x", space = "free")
 
 ggsave(paste0(g_drive, "results/osm/figures/moose_on-off_sqrt.png"), height = 4, width = 8, dpi = 500)
@@ -186,30 +193,36 @@ ggsave(paste0(g_drive, "results/osm/figures/moose_on-off_sqrt.png"), height = 4,
 
 buffer |>
   filter(common_name == "Moose") |>
+  mutate(lci_density = ifelse(mean_density > 0, lci_density, 0),
+         uci_density = ifelse(mean_density > 0, uci_density, 0)) |>
   ggplot(aes(x = fine_scale, y = mean_density)) +
-  geom_point(aes(color = vegetation),
+  geom_point(aes(color = Habitat),
              size = 3,
              position = position_dodge(width = 0.5)) +
-  geom_linerange(aes(ymin = lci_density, ymax = uci_density, color = vegetation),
+  geom_linerange(aes(ymin = lci_density, ymax = uci_density, color = Habitat),
                  linewidth = 0.5, position = position_dodge(width = 0.5)) +
   geom_point(data = (buffer_jem |> filter(common_name == "Moose")),
-             aes(x = fine_scale, y = mean_density, color = vegetation),
+             aes(x = fine_scale, y = mean_density, color = Habitat),
              size = 2,
              alpha = 0.2,
              position = position_jitterdodge(jitter.width = 0.1)) +
   scale_color_manual(values = veg.col) +
   scale_y_sqrt(breaks = c(0, 1, 2, 3)) +
-  theme_classic() +
+  theme_minimal() +
   labs(x = "Placement (metres away)",
        y = expression(Density~(individuals~per~km^2))) +
   coord_cartesian(ylim = c(0, 3)) +
   theme(axis.text.x = element_text(angle = 0, hjust = 0.5),
         axis.title.x = element_text(size = 14, margin = margin(0.4, 0, 0, 0, unit = "cm")),
-        axis.title.y = element_text(size = 12, margin = margin(0, 0.4, 0, 0, unit = "cm")),
+        axis.title.y = element_text(size = 10, margin = margin(0, 0.4, 0, 0, unit = "cm")),
         strip.text = element_text(size = 11),
         legend.position = "none",
+        legend.title = element_blank(),
+        legend.margin = margin(0, 0, 0, 0),
+        legend.spacing.x = unit(0.05, "cm"),
         axis.ticks = element_blank(),
-        panel.grid.major.y = element_line(linewidth = 0.5, color = "grey80")) +
+        panel.grid.major.y = element_line(linewidth = 0.5, color = "grey80"),
+        panel.grid.major.x = element_blank()) +
   facet_grid(. ~ treatment, scales = "free_x", space = "free")
 
 ggsave(paste0(g_drive, "results/osm/figures/moose_buffer_sqrt.png"), height = 4, width = 8, dpi = 500)
