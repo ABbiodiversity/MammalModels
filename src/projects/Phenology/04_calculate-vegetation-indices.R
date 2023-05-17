303ba0854c14b8332446e6e9fbb36e4e86b659c7
Marcus Becker <mabecker@ualberta.ca>
2023-05-17 17:32:18 -0600

    calculate vegetation indices

--- src/projects/Phenology/04_calculate-vegetation-indices.R
+++ src/projects/Phenology/04_calculate-vegetation-indices.R
@@ -18,39 +18,37 @@ library(stringr)
 #-----------------------------------------------------------------------------------------------------------------------
 
 # Locations
-sites <- c("252-SW", "1000-NE", "1146-NE", "1351-NW", "1400-NE", "1416-SE")
+sites <- c("1146-NE", "1416-SE", "252-SW", "965-SE", "1000-NE", "1351-NW", "1400-NE")
 
 for (site in sites) {
 
   extractVIs(img.path = paste0(g_drive, "projects/Phenology/Timelapse Images/", site, "/"),
-             roi.path = paste0(g_drive, "projects/Phenology/Outputs/ROI/", site, "_2020-06-01/"),
-             vi.path = paste0(g_drive, "projects/Phenology/Outputs/VI/"))
+             # Again, will have to be more nuanced here in the future in case the image date changes
+             roi.path = paste0(g_drive, "projects/Phenology/Outputs/ROI/", site, "_2020-06-01"),
+             vi.path = paste0(g_drive, "projects/Phenology/Outputs/VI/"),
+             roi.name = paste0(site, "_2020-06-01_roi1"),
+             plot = TRUE,
+             begin = NULL,
+             spatial = FALSE,
+             date.code = 'yyyy-mm-dd',
+             npixels = 1,
+             file.type = "jpg",
+             bind = FALSE)
 
+  # Rename the Rdata from generic VI.data.RData to a site-specific name
+  file.rename(from = paste0(g_drive, "projects/Phenology/Outputs/VI/VI.data.RData"),
+              to = paste0(g_drive, "projects/Phenology/Outputs/VI/", site, "_VI.data.RData"))
 
 }
 
-# Calculate veg indices
-VI.data <-extractVIs(paste0(g_drive, "projects/Phenology/Timelapse Images/AUB-12/Test/"), # image path
-                     paste0(g_drive, "projects/Phenology/Outputs/ROI/"), # roi path to get the stored roi
-                     vi.path = paste0(g_drive, "projects/Phenology/Outputs/VI/"), # where VI will be saved
-                     roi.name =  "roi", #name of the roi in the ROI.rdata
-                     plot = TRUE, begin = NULL, spatial = FALSE, date.code= 'yyyy-mm-dd', npixels = 1,
-                     file.type = ".jpg", bind = FALSE)
+# Make plots of each individual location
+load(paste0(g_drive, "projects/Phenology/Outputs/VI/1400-NE_VI.data.Rdata"))
 
-load(paste0(g_drive, "projects/Phenology/Outputs/VI/VI.data.Rdata"))
+with(VI.data$`1400-NE_2020-06-01_roi1`, plot(date, ri.av, pch=20, cex = 1.5, col='red', ylim=c(0.1,0.6),
+                                             ylab='Relative indexes', main = "1400-NE"))
+with(VI.data$`1400-NE_2020-06-01_roi1`, points(date, gi.av, col='green', pch=20, cex = 1.5))
+with(VI.data$`1400-NE_2020-06-01_roi1`, points(date, bi.av, col='blue', pch=20, cex = 1.5))
 
 
-with(VI.data$roi, plot(date, ri.av, pch=20, col='red', ylim=c(0.1,0.6), ylab='Relative indexes'))
-with(VI.data$roi, points(date, gi.av, col='green', pch=20))
-with(VI.data$roi, points(date, bi.av, col='blue', pch=20))
-
-library(purrr)
-library(magick)
-
-create_gif(images_folder = paste0(g_drive, "projects/Phenology/Timelapse Images/AUB-12/Test/GIF"),
-           file_type = "jpg",
-           fps = 2,
-           gif_name = "AUB-12-Pheno",
-           gif_folder = paste0(g_drive, "projects/Phenology"))
 
 
