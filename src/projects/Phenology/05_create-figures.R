6c783effff267134dfe074f88c0243aa7b82cc5b
Marcus Becker <mabecker@ualberta.ca>
2023-07-25 16:53:30 -0600

    almost made da gifs

--- src/projects/Phenology/05_create-figures.R
+++ src/projects/Phenology/05_create-figures.R
@@ -26,9 +26,30 @@ with(VI.data$`AUB-12_2020-02-03_roi1`, plot(date, ri.av, pch=20, cex = 1.5, col=
 with(VI.data$`AUB-12_2020-02-03_roi1`, points(date, gi.av, col='green', pch=20, cex = 1.5))
 with(VI.data$`AUB-12_2020-02-03`, points(date, bi.av, col='blue', pch=20, cex = 1.5))
 
+
+
 #-----------------------------------------------------------------------------------------------------------------------
 
 aub12 <- read_csv(paste0(g_drive, "projects/Phenology/Outputs/Fit/Export corrected RGB and spline AUB-12_2020-02-03.csv"))
+
+aub12_dates <- read_csv(paste0(g_drive, "projects/Phenology/Outputs/Fit/Camera phenology results subset 4 Dates July 19 2023.csv")) |>
+  filter(Session == "AUB-12_2020-02-03") |>
+  pivot_longer(cols = `Melt.Peak.Der`:`FirstSnow.Der`, names_to = "Metric", values_to = "Date") |>
+  mutate(Date = ymd(paste0(Year, "-", Date))) |>
+  select(Session, type = Metric, date = Date) |>
+  mutate(colour = case_when(
+    type == "Melt.Peak.Der" ~ "#00cdcd",
+    type == "Melt.Peak.Seg" ~ "#66cdaa",
+    type == "Melt.End.Seg" ~ "#e8e8e8",
+    type == "Greenup.20" ~ "#00ee00",
+    type == "Greenup.Peak" ~ "#3cb371",
+    type == "Greenup.80" ~ "#006400",
+    type == "Summer.Peak" ~ "#0000ff",
+    type == "Sen.20" ~ "#ffa500",
+    type == "Sen.Peak" ~ "#cd8500",
+    type == "Sen.80" ~ "#8b4500",
+    type == "FirstSnow.Der" ~ "1e90ff"
+  ))
 
 # AUB-12 images
 d <- df_files |>
@@ -81,15 +102,17 @@ create_gif(images_folder = paste0(g_drive, "projects/Phenology/Outputs/ROI/AUB-1
 library(ggplot2)
 library(gganimate)
 
-check <- aub12 |>
-  group_by(DOY1) |>
-  tally()
+aub12_bi.av <- VI.data[["AUB-12_2020-02-03_roi1"]] |>
+  select(date, bi.av.new = bi.av) |>
+  mutate(date = date(date))
 
 aub12_gif <- aub12 |>
   mutate(date = date(Date)) |>
   left_join(d, by = "date") |>
+  left_join(aub12_bi.av, by = "date") |>
   filter(!is.na(path)) |>
-  pivot_longer(cols = c(ri.av, gi.av, bi.av, pR, pG, pB), names_to = "name", values_to = "value") |>
+  select(-bi.av) |>
+  pivot_longer(cols = c(ri.av, gi.av, bi.av.new, pR, pG, pB), names_to = "name", values_to = "value") |>
   mutate(colour = case_when(
     str_detect(name, "r|R") ~ "Red",
     str_detect(name, "b|B") ~ "Blue",
@@ -102,30 +125,60 @@ aub12_gif <- aub12 |>
   dplyr::select(-name)
 
 aub12_lines <- aub12_gif |>
-  filter(type == "Prediction",
-         !colour == "Blue")
+  filter(type == "Prediction")
 
-aub12_points <- aub12_gif |>
+aub12_points_green <- aub12_gif |>
   filter(type == "Index",
-         !colour == "Blue")
+         colour == "Green")
 
-p <- ggplot() +
-  geom_point(data = aub12_points, aes(x = date, y = value, color = colour, group = date), size = 3, alpha = 0.5) +
-  #geom_line(data = aub12_lines, aes(x = date, y = value, color = colour), linewidth = 1.5) +
-  labs(title = 'Date: {frame_along}',
+aub12_points_blue <- aub12_gif |>
+  filter(type == "Index",
+         colour == "Blue")
+
+aub12_points_red <- aub12_gif |>
+  filter(type == "Index",
+         colour == "Red")
+
+p_dyn <- ggplot() +
+  geom_line(data = aub12_lines, aes(x = date, y = value, color = colour), linewidth = 1.5) +
+  geom_point(data = aub12_points_blue, aes(x = date, y = value, group = date), color = "#0000ff", size = 4, alpha = 0.2) +
+  geom_point(data = aub12_points_green, aes(x = date, y = value, group = date), color = "#00cd00", size = 4, alpha = 0.3) +
+  geom_point(data = aub12_points_red, aes(x = date, y = value, group = date), color = "#ffb5c5", size = 4, alpha = 0.5) +
+  labs(title = 'AUB-12: {frame_along}',
        x = "",
-       y = "") +
-  scale_color_manual(values = c("green", "red")) +
+       y = "Relative Proportion") +
+  scale_x_date(date_breaks = "2 months", date_labels = "%b %Y") +
+  scale_color_manual(values = c("#0000ff", "#00cd00", "#ffb5c5")) +
   transition_reveal(date, keep_last = TRUE) +
-  theme(legend.position = "none")
+  theme_minimal() +
+  theme(legend.position = "none",
+        axis.text.x = element_text(size = 12, angle = 45, hjust = 1),
+        axis.title.y = element_text(size = 12, margin = margin(0, 0.5, 0, 0, unit = "cm")))
 
-p
-
-animate(p, duration = 13, fps = 4)
+animate(p_dyn, duration = 13, fps = 4)
 
 gganimate::anim_save(filename = "aub-12.gif")
 
+# Static
+p_stat <- ggplot() +
+  geom_line(data = aub12_lines, aes(x = date, y = value, color = colour), linewidth = 1.5, alpha = 0.5) +
+  geom_point(data = aub12_points_blue, aes(x = date, y = value, group = date), color = "#0000ff", size = 4, alpha = 0.1) +
+  geom_point(data = aub12_points_green, aes(x = date, y = value, group = date), color = "#00cd00", size = 4, alpha = 0.15) +
+  geom_point(data = aub12_points_red, aes(x = date, y = value, group = date), color = "#ffb5c5", size = 4, alpha = 0.2) +
+  geom_vline(data = aub12_dates, aes(xintercept = date), color = aub12_dates$colour, linewidth = 1) +
+  labs(title = 'AUB-12',
+       x = "",
+       y = "Relative Proportion") +
+  scale_x_date(date_breaks = "2 months", date_labels = "%b %Y") +
+  scale_color_manual(values = c("#0000ff", "#00cd00", "#ffb5c5")) +
+  theme_minimal() +
+  theme(legend.position = "none",
+        axis.text.x = element_text(size = 12, angle = 45, hjust = 1),
+        axis.title.y = element_text(size = 12, margin = margin(0, 0.5, 0, 0, unit = "cm")))
 
+p_stat
+
+ggsave(filename = "aub-12.png")
 
 
 
